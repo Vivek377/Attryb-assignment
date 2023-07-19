8b7ef637ff1ef7569dd1762c69f7d3dd57779a9f
Vivek377 <vk640890@gmail.com>
2023-07-19 13:27:28 +0530

    middleware and inventory route added

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const connection = require("./db");
 const dealersRoute = require("./routes/dealers.route");
+const inventoryRoute = require("./routes/inventory.route");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/dealers", dealersRoute);
+app.use("/inventory", inventoryRoute);
 
 
 app.listen(process.env.PORT, async (req, res) => {
--- backend/models/inventory.model.js
+++ backend/models/inventory.model.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
+const DealerModel = require("./dealers.model")
 
-const inventorySchema = mongoose.Schema({
+const InventorySchema = mongoose.Schema({
     title: { required: true, type: String },
     image: { required: true, type: String },
-    price: { required: true, type: String },
+    price: { required: true, type: Number },
     colors: { required: true, type: Array },
     km: { required: true, type: Number },
     scratches: { required: true, type: Number },
     accidents: { required: true, type: Number },
     prevBuyers: { required: true, type: Number },
     originalPaint: { required: true, type: String },
-    registrationPlace: { required: true, type: String }
+    registrationPlace: { required: true, type: String },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: DealerModel }
 })
 
-const inventoryModel = mongoose.model("inventory", inventorySchema);
+const InventoryModel = mongoose.model("inventory", InventorySchema);
 
-module.exports = inventoryModel
+module.exports = InventoryModel
