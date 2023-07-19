e8864851c34d5adca1e9aa64cdda5a92bfbb2654
Vivek377 <vk640890@gmail.com>
2023-07-19 14:51:24 +0530

    OEM routes added

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const connection = require("./db");
 const dealersRoute = require("./routes/dealers.route");
 const inventoryRoute = require("./routes/inventory.route");
+const OEMRoute = require("./routes/OEM.route");
 require("dotenv").config();
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/dealers", dealersRoute);
 app.use("/inventory", inventoryRoute);
+app.use("/oem", OEMRoute);
 
 
 app.listen(process.env.PORT, async (req, res) => {
